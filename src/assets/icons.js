22c5ae83e476dbd30bad9a58af17a3e4cd7fe0f5
Jacob LaGesse <52800953+jacoblagesse@users.noreply.github.com>
2021-09-30 12:30:50 -0500

    Update icons.js

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,16 +1,21 @@
 export const icons  = [
   {
     id: 1,
+    image: require('./images/CGI.jpg'),
+    url: 'https://engineering.missouri.edu/research/research-centers/geospatial-intelligence-lab/'
+  },
+  {
+    id: 2,
     image: require('./images/nyt_logo.png'),
     url: 'https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html'
   },
   {
-    id: 2,
+    id: 3,
     image: require('./images/rji_logo.jpg'),
     url: 'https://www.rjionline.org/'
   },
   {
-    id: 3,
+    id: 4,
     image: require('./images/maneater_logo.png'),
     url: 'https://www.themaneater.com/'
   }
